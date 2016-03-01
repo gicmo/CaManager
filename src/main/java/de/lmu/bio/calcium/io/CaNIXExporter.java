28338a933016a9427998b8e17091f56ee3368871
Christian Kellner <christian@kellner.me>
2016-03-01 23:35:29 +0100

    NIXExport: save roi

--- src/main/java/de/lmu/bio/calcium/io/CaNIXExporter.java
+++ src/main/java/de/lmu/bio/calcium/io/CaNIXExporter.java
@@ -11,6 +11,7 @@ import ij.gui.Roi;
 import javax.vecmath.Point2d;
 import java.util.ArrayList;
 
+import ij.process.FloatPolygon;
 import org.g_node.nix.*;
 
 public class CaNIXExporter extends CaTask {
@@ -61,9 +62,9 @@ public class CaNIXExporter extends CaTask {
 
             fireTaskProgress(imageProcessed, imagesTotal, "Exporting image " + name);
 
-            Group g = b.createGroup(name, "image");
+            Group g = b.createGroup(name, "image.ca");
 
-            Section im = meta.createSection(name, "image");
+            Section im = meta.createSection(name, "image.ca");
             im.createProperty("creation_time", new Value(img.getCTime()));
             g.setMetadata(im);
 
@@ -90,16 +91,48 @@ public class CaNIXExporter extends CaTask {
             }
 
             NDSize shape = new NDSize(new int[]{m, n});
-            DataArray da = b.createDataArray(name + "-kymo", "kymograph.fg", DataType.Float, shape);
+            DataArray da = b.createDataArray(name + ".fg.kymo", "kymo.fg", DataType.Float, shape);
             da.setData(flat, shape, new NDSize(2, 0));
 
             g.addDataArray(da);
 
+            DataArray roiFg = saveRoiData(roi, name + ".roi.xy.fg", b);
+            if (roiFg.isInitialized()) {
+                g.addDataArray(roiFg);
+            }
         }
 
         fd.close();
     }
 
+
+    public DataArray saveRoiData(Roi roi, String name, Block parent) {
+        int roiType = roi.getType();
+
+        if (!(roiType == Roi.POLYLINE || roiType == Roi.FREELINE)) {
+            IJ.error("Unsupported ROI type.");
+            return new DataArray();
+        }
+
+        float strokeWidth = roi.getStrokeWidth();
+
+        FloatPolygon p = roi.getFloatPolygon();
+        int n = p.npoints;
+        float[] x = p.xpoints;
+        float[] y = p.ypoints;
+
+        DataArray da = parent.createDataArray(name, "roi.coordinates", DataType.Float, new NDSize(new int[]{2, n}));
+        da.setData(x, new NDSize(new int[]{1, n}), new NDSize(new int[]{0, 0}));
+        da.setData(y, new NDSize(new int[]{1, n}), new NDSize(new int[]{1, 0}));
+
+        Section root = parent.getMetadata();
+        Section meta = root.createSection(name, "roi");
+
+        meta.createProperty("type", new Value(roiType));
+        meta.createProperty("strokeWidth", new Value(strokeWidth));
+
+        return da;
+    }
 
 }
 
